e674543a6b1b2165fc00d8e9749a9d972ccf47ab
liximomo <lixi@mufengcm.com>
2018-03-02 16:40:20 +0800

    change api

--- src/recomputed.js
+++ src/recomputed.js
@@ -1,14 +1,14 @@
 import { createSelector } from 'reselect';
 
 function defaultPicker(object) {
-  return [object.props, object.state, object.context];
+  return [object.props, object.state];
 }
 
 function createComputedCreater() {
   return (instance, ...args) => {
     const selector = createSelector(...args);
 
-    const computed = () => selector(...defaultPicker(instance));
+    const computed = () => selector(...defaultPicker(instance), instance);
 
     return computed;
   };
